c079115dbcd11bc0ed09bb5c9bf3235e5bdd6e67
lm <405482116@qq.com>
2018-11-03 17:08:45 +0800

    implement table content

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,61 @@ const context = {
     "icon": "icon-life-bouy"
   }
   ],
-  body: "My first post. Wheeeee!"
+  table: [{
+    class: 'windows',
+    type: 'idle',
+    title: 'test123.123123.com',
+    info: '192.168.1.102',
+    folder: '/var/lib/....',
+    isDeny: true,
+    browserList: [{ name: 'Filefox' }, { name: 'Safari' }, { name: 'Ubuntu' }, { name: 'Chrome' }]
+
+  }, {
+    class: 'windows',
+    type: 'building',
+    title: 'test123.123123.com',
+    info: '192.168.1.103',
+    folder: '/var/lib/....',
+    isDeny: false,
+    browserList: [{ name: 'Filefox' }, { name: 'Safari' }, { name: 'Ubuntu' }, { name: 'Chrome' }]
+
+  }, {
+    class: 'ubuntu',
+    type: 'building',
+    title: 'test123.11111.com',
+    info: '192.168.1.104',
+    folder: '/var/lib/....',
+    isDeny: false,
+    browserList: [{ name: 'Filefox' }, { name: 'Safari' }]
+
+  }, {
+    class: 'debin',
+    type: 'building',
+    title: 'test123.11111.com',
+    info: '192.168.1.105',
+    folder: '/var/lib/....',
+    isDeny: false,
+    browserList: [{ name: 'Filefox' }, { name: 'Safari' }]
+
+  }, {
+    class: 'suse',
+    type: 'idle',
+    title: 'test123.11111.com',
+    info: '192.168.1.107',
+    folder: '/var/lib/....',
+    isDeny: false,
+    browserList: []
+
+  }, {
+    class: 'cent_os',
+    type: 'idle',
+    title: 'test123.11111.com',
+    info: '192.168.1.107',
+    folder: '/var/lib/....',
+    isDeny: true,
+    browserList: [{ name: 'Filefox' }, { name: 'Safari' }, { name: 'Ubuntu' }, { name: 'Chrome' }]
+
+  },]
 };
 /* GET home page. */
 router.get('/', function (req, res, next) {
